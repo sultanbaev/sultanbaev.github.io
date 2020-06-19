98356e28c9c097ef39ed1c4a8cc932e4fc1ddb5d
svvitaly <sultanbaev.vitaly@gmail.com>
2020-06-20 02:03:31 +0500

    Request number, date, source

--- ugok.js
+++ ugok.js
@@ -1,4 +1,7 @@
-const customName = document.getElementById('customname');
+const requestNumber = document.getElementById('requestnumber');
+//
+const requestDate = document.getElementById('requestdate');
+//
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
@@ -7,10 +10,15 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+//let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+let storyText = 'После поступления заявки №/_____ВВЕДИТЕ НОМЕР ЗАЯВКИ_____/ от FROMWHOM /_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/ года о периодической потере изображения с камеры наружного видеонаблюдения №60 была проведена диагностика оборудования и установлен выход из строя :inserty: вследствие :insertz:.';
+
+let insertX = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let insertY = ['контроллера видеопроцессора','матрицы','БП','камеры'];
+let insertZ = ['нестабильного питания','попадания влаги','аварии блока бесперебойного питания'];
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 randomize.addEventListener('click', result);
 
@@ -26,17 +34,29 @@ function result() {
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
 
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace('Bob',name);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  if(requestNumber.value !== '') {
+    const name = requestNumber.value;
+    newStory = newStory.replace('/_____ВВЕДИТЕ НОМЕР ЗАЯВКИ_____/', name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+  if(requestDate.value !== '') {
+    const name = requestDate.value;
+    newStory = newStory.replace('/_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/', name);
   }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  if(document.getElementById("pcn").checked) {
+    newStory = newStory.replace('FROMWHOM', 'ПЦН');
+  }
+
+  if(document.getElementById("batyr").checked) {
+    newStory = newStory.replace('FROMWHOM', 'Дежурной части Батыр');
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
