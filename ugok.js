9bd2606ad34bbead7a7d5fb5de89e46bc991be2d
svvitaly <sultanbaev.vitaly@gmail.com>
2020-06-20 03:19:16 +0500

    Random CCTV ready

--- ugok.js
+++ ugok.js
@@ -1,6 +1,7 @@
 const requestNumber = document.getElementById('requestnumber');
 //
 const requestDate = document.getElementById('requestdate');
+const camNumber = document.getElementById('camnumber');
 //
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -10,10 +11,12 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'После поступления заявки №/_____ВВЕДИТЕ НОМЕР ЗАЯВКИ_____/ от FROMWHOM /_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/ года о периодической потере изображения с камеры наружного видеонаблюдения №60 была проведена диагностика оборудования и установлен выход из строя :inserty: вследствие :insertz:.';
+//let storyText = 'После поступления заявки №/_____ВВЕДИТЕ НОМЕР ЗАЯВКИ_____/ от FROMWHOM /_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/ года о периодической потере изображения с камеры наружного видеонаблюдения №60 была проведена диагностика оборудования и установлен выход из строя :inserty: вследствие :insertz:.';
+
+let storyText = 'После поступления заявки №/_____ВВЕДИТЕ НОМЕР ЗАЯВКИ_____/ от FROMWHOM /_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/ года о :insertx: №/_____ВВЕДИТЕ НОМЕР КАМЕРЫ_____/ была проведена диагностика оборудования и установлен выход из строя :inserty: вследствие :insertz:.';
 
 let insertX = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
-let insertY = ['контроллера видеопроцессора','матрицы','БП','камеры'];
+let insertY = ['контроллера видеопроцессора','матрицы','блока питания','камеры'];
 let insertZ = ['нестабильного питания','попадания влаги','аварии блока бесперебойного питания'];
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,8 +30,8 @@ function result() {
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
 
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
+  //newStory = newStory.replace(':insertx:',xItem);
+ // newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':insertz:',zItem);
 
@@ -43,6 +46,24 @@ function result() {
     const name = requestDate.value;
     newStory = newStory.replace('/_____ВВЕДИТЕ ДАТУ ПОСТУПЛЕНИЯ ЗАЯВКИ_____/', name);
   }
+
+  if(camNumber.value !== '') {
+    const name = camNumber.value;
+    newStory = newStory.replace('/_____ВВЕДИТЕ НОМЕР КАМЕРЫ_____/', name);
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var e = document.getElementById("ddlViewBy");
+var value = e.options[e.selectedIndex].value;
+//var text = e.options[e.selectedIndex].text;
+
+if (value == 'none') {newStory = newStory.replace(':insertx:','/_____ВЫБЕРИТЕ ОПИСАНИЕ НЕИСПРАВНОСТИ_____/');}
+if (value == 'noimage') {newStory = newStory.replace(':insertx:','потере изображения с камеры видеонаблюдения');}
+if (value == 'noimageperiodically') {newStory = newStory.replace(':insertx:','периодической потере изображения с камеры видеонаблюдения');}
+if (value == 'imagescrambled') {newStory = newStory.replace(':insertx:','помехах при выводе изображения с камеры видеонаблюдения');}
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
